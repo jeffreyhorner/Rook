24fea1741c330008bbd2ee2a6c4e8aba39824126
Jeffrey Horner <jeffrey.horner@gmail.com>
2011-04-26 08:03:04 -0500

    Typo in assignment operator

--- Rook/inst/exampleApps/helloworldref.R
+++ Rook/inst/exampleApps/helloworldref.R
@@ -1,12 +1,12 @@
 app <- setRefClass(
-    'HelloWorld',
+    'HelloWorld'
     methods = list(
 	call = function(env){
 	    list(
-		status=200,
-		headers <- list(
+		status=200
+		headers = list(
 		    'Content-Type' = 'text/html'
-		    ),
+		    )
 		body = paste('<h1>Hello World!</h1>')
 	    )
 	}
