75bfa2bd7be2285e7dab4e5065da6e1fa56eda6d
evanbiederstedt <evan.biederstedt@gmail.com>
2022-11-04 03:06:51 -0400

    add PROTECT()/UNPROTECT() to allocVector() calls

--- src/rook.c
+++ src/rook.c
@@ -27,10 +27,15 @@ SEXP rawmatch( SEXP needle, SEXP haystack, SEXP allMatches){
       error_return("all must be a logical vector")
          all = LOGICAL(allMatches)[0];
 
-   if (n2 > n1) return allocVector(INTSXP,0);
+   if (n2 > n1) {
+      SEXP result = PROTECT(allocVector(INTSXP,0));
+      UNPROTECT(1);
+
+      return result;
+   }
 
    k = 0;
-   ans = allocVector(INTSXP, all? (int)(n1 / n2) : 1);
+   ans = PROTECT(allocVector(INTSXP, all? (int)(n1 / n2) : 1));
 
    for (i = 0; i < n1; i++){
       if (x1[i] == x2[0]){
@@ -46,8 +51,10 @@ SEXP rawmatch( SEXP needle, SEXP haystack, SEXP allMatches){
    }
    if (k == LENGTH(ans)) return ans;
 
-   newans = allocVector(INTSXP,k);
+   newans = PROTECT(allocVector(INTSXP,k));
    while(k) {k--;INTEGER(newans)[k] = INTEGER(ans)[k];}
+
+   UNPROTECT(2);
    return newans;
 }
 
